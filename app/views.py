71faf461d44dc63c8b4fffe86267698fd1a85b56
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-03-09 20:07:48 +0100

    added views to handle url requests from urls.py

--- app/views.py
+++ app/views.py
@@ -1,2 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
+
+from app.models import Auto, Make
+from app.forms import MakeForm
+
+class MainView(LoginRequiredMixin, View):
+    def get(self, request):
+        mc = Make.objects.all().count()
+        al = Auto.objects.all()
+        ctx = { 'make_count' : mc, 'auto_list' : al }
+
+        return render(request, 'app/auto_list.html', ctx)
+
+
+class MakeView(LoginRequiredMixin, View):
+    def get(self, request):
+        ml = Make.objects.all()
+        ctx = { 'make_list' : ml }
+        return render(request, 'app/make_list.html', ctx)
+
+
+class MakeCreate(LoginRequiredMixin, CreateView):
+    model = Make
+    success_url = reverse_lazy('app:all')
+    fields = '__all__'
+
+
+class MakeUpdate(LoginRequiredMixin, UpdateView):
+    model = Make
+    success_url = reverse_lazy('app:all')
+    fields = '__all__'
+
+
+class MakeDelete(LoginRequiredMixin, DeleteView):
+    model = Make
+    success_url = reverse_lazy('app:all')
+    fields = '__all__'
+
+
+class AutoCreate(LoginRequiredMixin, CreateView):
+    model = Auto
+    fields = '__all__'
+    success_url = reverse_lazy('app:all')
+
+
+class AutoUpdate(LoginRequiredMixin, UpdateView):
+    model = Auto
+    fields = '__all__'
+    success_url = reverse_lazy('app:all')
+
+
+class AutoDelete(LoginRequiredMixin, DeleteView):
+    model = Auto
+    fields = '__all__'
+    success_url = reverse_lazy('app:all')
+
