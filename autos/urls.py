1427cc71d07ec47f0e05fa4ab169d688195026e0
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-03-12 16:54:48 +0100

    added namespace for 'app'

--- autos/urls.py
+++ autos/urls.py
@@ -20,6 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('app/', include('app.urls')), 
+    path('app/', include('app.urls', namespace='app')),
 
 ]
